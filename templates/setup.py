e57beea6d1128b26e38ffa7891365ae3dcafbf2b
alice-dev <takemehighermore@gmail.com>
2017-03-16 06:06:41 +0000

    Add file: the template text files

---
+++ templates/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from distutils.core import setup
+
+with open("LICENSE", "r") as fp:
+    license_msg = fp.read()
+
+setup(
+    name="",
+    description="",
+    author="",
+    version="",
+    license=license_msg,
+    classifiers=[
+    ]
+)
