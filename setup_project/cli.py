98bcec7e92256bdb9c0bfa12ee6a611f5dc02c04
alice-dev <takemehighermore@gmail.com>
2017-03-17 06:44:57 +0000

    Removed optional arguments: '-f', '-d'

--- setup_project/cli.py
+++ setup_project/cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from argparse import ArgumentParser, SUPPRESS
+from argparse import ArgumentParser
 
 
 parser = ArgumentParser(
@@ -18,20 +18,6 @@ parser.add_argument(
     "project",
     action="store",
     help="The project name")
-
-parser.add_argument(
-    "-f", "--formula-path",
-    action="store",
-    dest="formula_path",
-    metavar="PATH",
-    help="You can set your original formula. please write formula path.")
-
-parser.add_argument(
-    "-d", "--under-develop",
-    action="store_true",
-    dest="develop",
-    default=True,
-    help=SUPPRESS)
 
 parser.add_argument(
     "-l", "--list",
