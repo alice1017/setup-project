0e03fc76f7f658c627baf92b01337835213f2171
alice-dev <takemehighermore@gmail.com>
2017-03-17 06:46:45 +0000

    Changed function name & Implemented execute script process

--- setup_project/__main__.py
+++ setup_project/__main__.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 import sys
 
 from .cli import parser
+from .core import execute_script
+from .utils import ScriptManager
 
-def execute(args):
+
+def program(args):
+
+    manager = ScriptManager()
+
+    if args.formula_list:
+
+        print "Formulas:"
+
+        for key in manager.data_cache:
+            print "  * {0}".format(key)
+
+        sys.exit(0)
+
+    script_path = manager.get_script(args.formula)
+    execute_script(script_path, args.project)
 
     return 0
 
@@ -22,7 +38,7 @@ def main(argv=sys.argv):
 
     args = parser.parse_args(argv)
 
-    exit_code = execute(args)
+    exit_code = program(args)
     return exit_code
 
 
